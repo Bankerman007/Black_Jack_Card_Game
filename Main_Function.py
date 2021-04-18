4bc6e6903cf91d03a9ef3f03164cc325ce1c0adc
Bankerman007 <scottwright.qrf@gmail.com>
2021-04-18 14:45:23 -0500

    Black Jack Execution

---
+++ Main_Function.py
@@ -0,0 +1,13 @@
+from blackjackmain import Blackjack
+from deck import Deck
+from player_class import Player
+
+
+def main():
+    b = Blackjack()
+    b.play()
+    
+
+if __name__ == "__main__":
+    main()
+    
